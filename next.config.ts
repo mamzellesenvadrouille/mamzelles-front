bdd5b85d3450255238eda80afa8e981866b3f9ac
mamzellesenvadrouille <mamzellesenvadrouille@gmail.com>
2026-06-01 13:54:03 +0200

    deploy

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async redirects() {
     return [
-      // Nouvelles URLs
-      { source: '/visiter-madrid-en-3-jours', destination: 'https://blog.mamzellesenvadrouille.com/visiter-madrid-en-3-jours', permanent: false },
-      { source: '/visiter-madrid-en-3-jours/', destination: 'https://blog.mamzellesenvadrouille.com/visiter-madrid-en-3-jours/', permanent: false },
       { source: '/dormir-dans-le-desert-agafay', destination: 'https://blog.mamzellesenvadrouille.com/dormir-dans-le-desert-agafay', permanent: true },
       { source: '/dormir-dans-le-desert-agafay/', destination: 'https://blog.mamzellesenvadrouille.com/dormir-dans-le-desert-agafay/', permanent: true },
       { source: '/visiter-marrakech', destination: 'https://blog.mamzellesenvadrouille.com/visiter-marrakech', permanent: true },
@@ -25,9 +22,8 @@ const nextConfig: NextConfig = {
       { source: '/destinations/:path*', destination: 'https://blog.mamzellesenvadrouille.com/destinations/:path*', permanent: true },
       { source: '/formulaire-de-contact', destination: '/formules#formulaire', permanent: true },
       { source: '/formulaire-de-contact/', destination: '/formules#formulaire', permanent: true },
-      // Anciennes URLs signalées par Google
-      { source: '/madrid-en-3-jours', destination: 'https://blog.mamzellesenvadrouille.com/visiter-madrid-en-3-jours', permanent: true },
-      { source: '/madrid-en-3-jours/', destination: 'https://blog.mamzellesenvadrouille.com/visiter-madrid-en-3-jours/', permanent: true },
+      { source: '/madrid-en-3-jours', destination: '/visiter-madrid-en-3-jours', permanent: true },
+      { source: '/madrid-en-3-jours/', destination: '/visiter-madrid-en-3-jours/', permanent: true },
       { source: '/que-voir-a-marrakech', destination: 'https://blog.mamzellesenvadrouille.com/visiter-marrakech', permanent: true },
       { source: '/que-voir-a-marrakech/', destination: 'https://blog.mamzellesenvadrouille.com/visiter-marrakech/', permanent: true },
       { source: '/partir-une-semaine-a-londres', destination: 'https://blog.mamzellesenvadrouille.com/visiter-londres-en-1-semaine', permanent: true },
