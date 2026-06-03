d03b69086f6103e5a8a04c58a747eeff331da8be
mamzellesenvadrouille <mamzellesenvadrouille@gmail.com>
2026-06-03 19:59:16 +0200

    deploy

--- app/visiter-madrid-en-3-jours/page.tsx
+++ app/visiter-madrid-en-3-jours/page.tsx
@@ -412,6 +412,7 @@ export default function MadridPage() {
 
             <div className="article-comments-list">
 
+              <div className="article-comment-thread">
               <div className="article-comment">
                 <div className="article-comment-avatar">S</div>
                 <div className="article-comment-body">
@@ -432,7 +433,9 @@ export default function MadridPage() {
                   <p className="article-comment-text">Coucou ! On découvre seulement aujourd&apos;hui votre petit message qui nous fait énormément plaisir ! 🥰 Heureuses de savoir que notre article ait pu vous inspirer pour votre séjour à Madrid. Merci beaucoup à vous aussi pour votre retour ! 😊</p>
                 </div>
               </div>
+              </div>
 
+              <div className="article-comment-thread">
               <div className="article-comment">
                 <div className="article-comment-avatar">T</div>
                 <div className="article-comment-body">
@@ -453,7 +456,9 @@ export default function MadridPage() {
                   <p className="article-comment-text">Trop contentes d&apos;avoir contribué à votre organisation ! 🤗🤗 Cette ville est incroyable, on a tellement aimé ! Bons préparatifs et bon voyage. ✈️😊</p>
                 </div>
               </div>
+              </div>
 
+              <div className="article-comment-thread">
               <div className="article-comment">
                 <div className="article-comment-avatar">N</div>
                 <div className="article-comment-body">
@@ -474,7 +479,9 @@ export default function MadridPage() {
                   <p className="article-comment-text">Bonjour, merci pour votre retour ! 🤗 Joyeux anniversaire à votre garçon ! 🎉 On vous souhaite un magnifique séjour dans cette ville incroyable ! 🤩</p>
                 </div>
               </div>
+              </div>
 
+              <div className="article-comment-thread">
               <div className="article-comment">
                 <div className="article-comment-avatar">V</div>
                 <div className="article-comment-body">
@@ -495,7 +502,9 @@ export default function MadridPage() {
                   <p className="article-comment-text">Bonjour, non malheureusement, nous n&apos;avons pas de logement à recommander. En espérant que vous avez trouvé votre bonheur ! 🤗 Belles fêtes de fin d&apos;année et bon séjour à Madrid ! ☀️</p>
                 </div>
               </div>
+              </div>
 
+              <div className="article-comment-thread">
               <div className="article-comment">
                 <div className="article-comment-avatar">G</div>
                 <div className="article-comment-body">
@@ -516,7 +525,9 @@ export default function MadridPage() {
                   <p className="article-comment-text">Top ! Merci pour ce retour qui fait super plaisir ! 🤗🤗</p>
                 </div>
               </div>
+              </div>
 
+              <div className="article-comment-thread">
               <div className="article-comment">
                 <div className="article-comment-avatar">D</div>
                 <div className="article-comment-body">
@@ -536,6 +547,7 @@ export default function MadridPage() {
                   </div>
                   <p className="article-comment-text">Bonjour, olala... On découvre seulement votre message aujourd&apos;hui ! 🙈 Votre retour sur notre article nous fait énormément plaisir ! 🤗 On vous souhaite également une belle continuation et on espère que vous en avez aussi pris plein les yeux à Madrid ! ☀️</p>
                 </div>
+              </div>
               </div>
 
             </div>
