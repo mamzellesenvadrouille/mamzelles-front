b16b10bb9ba19b6b59c83a7a8684e63cd999ccb5
mamzellesenvadrouille <mamzellesenvadrouille@gmail.com>
2026-06-09 13:39:37 +0200

    deploy

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,28 +3,34 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.mamzellesenvadrouille.com',
+      url: 'https://mamzellesenvadrouille.com',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://www.mamzellesenvadrouille.com/formules',
+      url: 'https://mamzellesenvadrouille.com/formules',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'https://www.mamzellesenvadrouille.com/qui-sommes-nous',
+      url: 'https://mamzellesenvadrouille.com/qui-sommes-nous',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://www.mamzellesenvadrouille.com/blog',
+      url: 'https://mamzellesenvadrouille.com/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
+    },
+    {
+      url: 'https://mamzellesenvadrouille.com/visiter-madrid-en-3-jours',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
     },
   ];
 }
