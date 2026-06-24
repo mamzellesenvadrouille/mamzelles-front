148b086f268a94487c6dbab988d248622b4bf96b
mamzellesenvadrouille <mamzellesenvadrouille@gmail.com>
2026-06-24 13:02:30 +0200

    deploy

--- app/visiter-marrakech/page.tsx
+++ app/visiter-marrakech/page.tsx
@@ -74,7 +74,7 @@ export default function MarrakechPage() {
         <div className="hero-content">
           <h1>Marrakech<br /><em>Que faire dans la ville rouge</em></h1>
           <div className="hero-rule" />
-          <p className="hero-desc">Tous nos secrets pour découvrir<br className="mobile-only-br" />la ville rouge<br className="tablet-only-br" /> et son effervescence.</p>
+          <p className="hero-desc">Tous nos secrets pour découvrir<br className="marrakech-hero-br" />la ville rouge<br className="tablet-only-br" /> et son effervescence.</p>
         </div>
       </section>
 
