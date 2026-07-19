a4b93b82e3728ca43e92106681242695f4dbdad7
mamzellesenvadrouille <mamzellesenvadrouille@gmail.com>
2026-07-19 21:30:17 +0200

    deploy

--- app/formules/page.tsx
+++ app/formules/page.tsx
@@ -314,7 +314,7 @@ export default function Formules() {
                 </div>
               </div>
               <h3>Réception de votre<br />carnet interactif</h3>
-              <p>Itinéraire(s), liens de réservation,<br />cartes... tout-en-un.</p>
+              <p>Itinéraire(s), liens de réservation,<br />cartes Maps... tout-en-un.</p>
             </div>
             <div className="process-arrow">›</div>
           </div>
