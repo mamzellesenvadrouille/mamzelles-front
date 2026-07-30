19764e72c823066466bda5c591d54037cdbe0217
mamzellesenvadrouille <mamzellesenvadrouille@gmail.com>
2026-07-30 20:12:23 +0200

    deploy

--- app/voyage/[slug]/DestinationTabs.tsx
+++ app/voyage/[slug]/DestinationTabs.tsx
@@ -262,9 +262,8 @@ export default function DestinationTabs({
 
         {(dest.hebergements ?? []).length > 0 && (
           <>
-            <div className={styles.sectionLabelBig} style={{ display: "flex", alignItems: "center", gap: 10 }}>
-              <span style={{ width: 6, height: 6, borderRadius: "50%", background: "#1a1512", flexShrink: 0 }} />
-              Hébergement{(dest.hebergements ?? []).length > 1 ? "s" : ""}
+            <div className={styles.sectionLabelBig}>
+              Votre <em style={{ fontStyle: "italic" }}>hébergement</em>
             </div>
             <div className={styles.miniGrid}>
               {(dest.hebergements ?? []).map((h, i) => {
@@ -309,9 +308,8 @@ export default function DestinationTabs({
 
         {dest.activites.length > 0 && (
           <>
-            <div className={styles.sectionLabelBig} style={{ display: "flex", alignItems: "center", gap: 10 }}>
-              <span style={{ width: 6, height: 6, borderRadius: "50%", background: "#1a1512", flexShrink: 0 }} />
-              {dest.activites.length} site{dest.activites.length > 1 ? "s" : ""} & activité{dest.activites.length > 1 ? "s" : ""}
+            <div className={styles.sectionLabelBig}>
+              {dest.activites.length} site{dest.activites.length > 1 ? "s" : ""} & <em style={{ fontStyle: "italic" }}>activité{dest.activites.length > 1 ? "s" : ""}</em>
             </div>
             <div className={styles.miniGrid}>
               {dest.activites.map((a, i) => {
@@ -386,9 +384,8 @@ export default function DestinationTabs({
 
         {dest.restaurants.length > 0 && (
           <>
-            <div className={styles.sectionLabelBig} style={{ display: "flex", alignItems: "center", gap: 10 }}>
-              <span style={{ width: 6, height: 6, borderRadius: "50%", background: "#1a1512", flexShrink: 0 }} />
-              {dest.restaurants.length} restaurant{dest.restaurants.length > 1 ? "s" : ""} recommandé{dest.restaurants.length > 1 ? "s" : ""}
+            <div className={styles.sectionLabelBig}>
+              {dest.restaurants.length} restaurant{dest.restaurants.length > 1 ? "s" : ""} <em style={{ fontStyle: "italic" }}>recommandé{dest.restaurants.length > 1 ? "s" : ""}</em>
             </div>
             <div className={styles.miniGrid}>
               {dest.restaurants.map((r, i) => {
