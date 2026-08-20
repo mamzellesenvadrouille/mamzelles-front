bac6d93771d76ef027e2192d031f142c204f503f
mamzellesenvadrouille <mamzellesenvadrouille@gmail.com>
2026-08-20 20:32:27 +0200

    deploy

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,9 @@ const nextConfig: NextConfig = {
       { source: '/palawan-autrement-de-puerto-princesa-a-port-barton-hors-des-sentiers-battus', destination: '/palawan-de-puerto-princesa-a-port-barton-hors-des-sentiers-battus', permanent: true },
       { source: '/palawan-autrement-de-puerto-princesa-a-port-barton-hors-des-sentiers-battus/', destination: '/palawan-de-puerto-princesa-a-port-barton-hors-des-sentiers-battus', permanent: true },
       { source: '/palawan-de-puerto-princesa-a-port-barton-hors-des-sentiers-battus/', destination: '/palawan-de-puerto-princesa-a-port-barton-hors-des-sentiers-battus', permanent: true },
+      { source: '/destinations', destination: '/blog', permanent: true },
+      { source: '/destinations/', destination: '/blog', permanent: true },
+      { source: '/destinations/:path*', destination: '/blog', permanent: true },
     ]
   }
 };
