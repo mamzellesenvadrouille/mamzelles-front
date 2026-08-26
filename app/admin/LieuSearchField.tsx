9a71e1b79bdba3dfdb6774d03ee35fe2f6cd33ff
mamzellesenvadrouille <mamzellesenvadrouille@gmail.com>
2026-08-26 09:04:25 +0200

    deploy

--- app/admin/LieuSearchField.tsx
+++ app/admin/LieuSearchField.tsx
@@ -150,7 +150,7 @@ export default function LieuSearchField({
 
   function choisir(r: Resultat) {
     onSelect(r);
-    setSaisie(r.nom);
+    setSaisie("");
     setLieuChoisi(r);
     setResultats([]);
     setOuvert(false);
